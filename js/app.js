772e6d7f9376eb71a670cb7e0c72b42b9a8baa4d
Deyan Bozhilov <dodobrat98@gmail.com>
2020-09-09 11:49:48 +0300

    change: 👽 Parallax, mobile expandable navbar, sizing adjustments

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
 document.querySelector('#copyright-year').innerHTML = new Date().getFullYear()
+
+const navbar = document.querySelector('.navbar');
+const navbarToggler = document.querySelector('.navbar__mobile__toggle');
+const heroImage = document.querySelector('.hero__img');
+
+const shrinkNavbar = () => {
+    navbar.classList.add('navbar--scrolled');
+}
+
+const expandNavbar = () => {
+    navbar.classList.remove('navbar--scrolled');
+}
+
+const toggleMobileNavbar = () => {
+    navbar.classList.toggle('navbar--toggled');
+}
+
+window.addEventListener('scroll', () => {
+    if(window.pageYOffset > 100){
+        shrinkNavbar()
+    }else{
+        expandNavbar()
+    }
+    heroImage.style.bottom = `${window.pageYOffset * -0.5}px`
+})
+
+navbarToggler.addEventListener('click', toggleMobileNavbar);
